9506f87ce6e0d7bb30a240145af35211e31cb650
dcikadaiss <daissdcika@gmail.com>
2025-10-04 12:40:22 +0000

    second commit

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,6 +2,16 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('Test endpoint /', () => {
+  let server;
+
+  beforeAll((done) => {
+    server = app.listen(3000, done);
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
   it('should return 200 and Hello message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
