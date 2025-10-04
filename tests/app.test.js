8ae32401a8be07664c7bc289883cd6b23057a4b3
dcikadaiss <daissdcika@gmail.com>
2025-10-04 12:44:27 +0000

    second commit

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,19 +1,8 @@
+// tests/app.test.js
 const request = require('supertest');
-const app = require('../app');
+const app = require('../index'); // ← Import correct de index.js
 
 describe('Test endpoint /', () => {
-  let server;
-
-  beforeAll(async () => {
-    server = app.listen(3000);
-    // Attendre que le serveur soit prêt
-    await new Promise(resolve => server.once('listening', resolve));
-  });
-
-  afterAll(async () => {
-    await new Promise(resolve => server.close(resolve));
-  });
-
   it('should return 200 and Hello message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
